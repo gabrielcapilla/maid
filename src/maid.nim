6fcc9e62967ac9962ef87d4dae0c48919e57b250
Gabriel Capilla <170623949+gabrielcapilla@users.noreply.github.com>
2024-06-16 02:44:17 +0200

    feat: Add new calls/flags

--- src/maid.nim
+++ src/maid.nim
@@ -1,24 +1,30 @@
 import os
 import flags/flags
-import fn/[cacheClean, cacheThumbnails, currentVersion]
+import fn/[cacheClean, cacheThumbnails, currentVersion, clearHistory, clearClipboard]
 
-proc processFlags(params: seq[string]): tuple[cache: bool, thumbnails: bool, help: bool, version: bool] =
-  (flagCache in params, flagThumbnails in params, flagHelp in params, flagVersion in params)
+proc processFlags(params: seq[string]): tuple[history: bool, clipboard: bool, thumbnails: bool, clear: bool, help: bool, version: bool] =
+  (flagHistory in params, flagClipboard in params, flagThumbnails in params, flagClear in params, flagHelp in params, flagVersion in params)
 
 proc main() =
   let params: seq[string] = commandLineParams()
-  let (cache, thumbnails, help, version) = processFlags(params)
+  let (history, clipboard, thumbnails, clear, help, version) = processFlags(params)
 
   if help:
     flags.print()
   if version:
     currentVersion.print()
-  if cache:
-    cacheClean.clean()
+  if history:
+    clearHistory.clean()
+  if clipboard:
+    clearClipboard.clean()
   if thumbnails:
     cacheThumbnails.clean()
+  if clear:
+    cacheClean.clean()
+    clearHistory.clean()
+    clearClipboard.clean()
 
-  if not (cache or thumbnails or help or version):
+  if not (clear or history or clipboard or thumbnails or help or version):
     flags.print()
 
 main()
