a5a28fe01cc9d4d32fc32b91d94bd3cccd7ebbd1
Gabriel Capilla <170623949+gabrielcapilla@users.noreply.github.com>
2024-06-16 02:42:31 +0200

    style: Add new flags

--- src/flags/flags.nim
+++ src/flags/flags.nim
@@ -1,8 +1,10 @@
 const
   flagHelp*: string =       "--help"
-  flagCache*: string =      "--cache"
+  flagClear*: string =      "--clear"
   flagVersion*: string =    "--version"
-  flagThumbnails*: string = "--thumbnails"
+  flagHistory*: string =    "-h"
+  flagClipboard*: string =  "-k"
+  flagThumbnails*: string = "-t"
 
 proc print*() =
-  echo "Usage: maid [", flagCache, "] [", flagThumbnails, "] [", flagHelp, "] [", flagVersion, "]"
+  echo "Usage: maid [command] \n\n ", flagClipboard, " \t| Clears the clipboard\n ", flagThumbnails, " \t| Clears all cached thumbnails\n ", flagHistory, " \t| Clear the history of commands (bash & fish)\n\n ", flagHelp, " \t| Displays available commands\n ", flagVersion, " \t| Show the version of the utility\n ", flagClear, " \t| Clear ALL unwanted tracers users leave on the system"
