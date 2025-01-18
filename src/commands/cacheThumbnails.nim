c4da08f1f6e36db6ce89c9597cd5d109ccf68e1d
Gabriel Capilla <170623949+gabrielcapilla@users.noreply.github.com>
2025-01-18 15:50:51 +0100

    refactor: Better coding, error handling with OSError and more

--- src/commands/cacheThumbnails.nim
+++ src/commands/cacheThumbnails.nim
@@ -1,18 +1,25 @@
 import std/os
 
 const
-  cacheDir = joinPath(getHomeDir(), ".cache", "thumbnails")
-  dirNames = ["large", "normal", "x-large", "xx-large"]
+  HomeDir: string = getHomeDir()
+  cacheDir: string = joinPath(HomeDir, ".cache", "thumbnails")
+  dirNames: array[0..3, string] = ["large", "normal", "x-large", "xx-large"]
 
 proc cleanThumbnails*() =
+  ## Cleans the thumbnail cache by removing all files in the specified subdirectories.
+
   if dirExists(cacheDir):
     for dirName in dirNames:
       let dirPath = joinPath(cacheDir, dirName)
       if dirExists(dirPath):
+        # Remove all files in the subdirectory
         for file in walkFiles(dirPath / "*"):
-          removeFile(file)
-        echo "Thumbnail cache in ", dirPath, " removed."
+          try:
+            removeFile(file)
+          except OSError as e:
+            stderr.writeLine("Error deleting ", file, ": ", e.msg)
+        stdout.writeLine("Thumbnail cache in ", dirPath, " removed.")
       else:
-        echo "The folder ", dirPath, " does not exist."
+        stderr.writeLine("The folder ", dirPath, " does not exist.")
   else:
-    echo "The folder ", cacheDir, " does not exist."
+    stderr.writeLine("The folder ", cacheDir, " does not exist.")
